49754a53f3c72f7426fe60299ad3157bdf20eddc
Scott Reese <scott_reese@pm.me>
2019-09-29 18:00:09 -0400

    assessment meets requirements

--- src/components/main.js
+++ src/components/main.js
@@ -1,28 +1,58 @@
 import React, { useState, useEffect } from "react"
+import { Form, Button, Container, Row } from "react-bootstrap"
+import Cookies from "universal-cookie"
 import { api } from "./constants"
 
-function NavBar() {
+function NavBar(props) {
   return (
-    <nav class="nav mb-3 justify-content-center">
-      <a class="nav-link" href="/">
-        Index
-      </a>
-      <a class="nav-link" href="post">
-        New Post
-      </a>
-      <a class="nav-link" href="boasts">
+    <Container fluid>
+      <Row className="mb-3 justify-content-md-center">
+      <Button
+        className = "mr-2"
+        onClick={e => {
+          e.preventDefault()
+          props.handleAllPosts()
+        }}
+      >
+        All
+      </Button>
+      <Button
+        className = "mr-2"
+        onClick={e => {
+          e.preventDefault()
+          props.handleFilterPosts(true)
+        }}
+      >
         Boasts
-      </a>
-      <a class="nav-link" href="roasts">
+      </Button>
+      <Button
+        className = "mr-2"
+        onClick={e => {
+          e.preventDefault()
+          props.handleFilterPosts(false)
+        }}
+      >
         Roasts
-      </a>
-      <a class="nav-link" href="topvoted">
+      </Button>
+      <Button
+        className = "mr-2"
+        onClick={e => {
+          e.preventDefault()
+          props.handleSort(true)
+        }}
+      >
         Sort by Top Voted
-      </a>
-      <a class="nav-link" href="leastvoted">
+      </Button>
+      <Button
+        onClick={e => {
+          e.preventDefault()
+          props.handleSort(false)
+        }}
+      >
         Sort by Least Voted
-      </a>
-    </nav>
+      </Button>
+      </Row>
+    </Container>
   )
 }
 
@@ -54,6 +84,69 @@ function PostHeader(props) {
   )
 }
 
+function NewPostForm(props) {
+  const [text, setText] = useState("")
+
+  function handlePostForm(event) {
+    event.preventDefault()
+
+    const data = new FormData(event.target)
+
+    let text = data.get('text')
+    let is_boast = data.get('isRoast') === 'on' ? 1 : 0
+    
+    event.target[0].value = ''
+    setText('')
+    fetch(api + 'post', {
+      method: 'POST',
+      body: JSON.stringify({
+        text,
+        is_boast
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      props.handleNewPost(res)
+    })
+  }
+
+  return (
+    <Container className="border rounded pr-4 pl-4 pt-4 mb-4">
+      <Form onSubmit={e => handlePostForm(e)}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>
+            What are you thinking? Or what grinds your gears?
+          </Form.Label>
+          <Form.Control
+            onChange={e => {
+              e.preventDefault()
+              setText(e.target.value)
+            }}
+            rows="3"
+            style={{resize: "none"}}
+            type="text"
+            name="text"
+            placeholder=""
+            maxlength="280"
+          />
+          {text.length <= 280 ? (
+            <Form.Text className="text-muted">
+              {280 - text.length} characters remaining...
+            </Form.Text>
+          ) : (
+            <Form.Text className="text-danger">
+              {280 - text.length} characters remaining...
+            </Form.Text>
+          )}
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Is Roast" name="isRoast"/>
+        </Form.Group>
+      </Form>
+    </Container>
+  )
+}
+
 function PostBody(props) {
   return (
     <React.Fragment>
@@ -61,15 +154,37 @@ function PostBody(props) {
       <div class="btn-toolbar ml-3 mb-3" role="toolbar">
         <div class="input-group mr-3">
           <div class="input-group-prepend">
-            <form method="POST" action="upvote/?id={{ post.id }}">
-              <button type="submit" class="btn btn-success">
+            <form method="GET">
+              <button
+                type="submit"
+                class="btn btn-success"
+                onClick={e => {
+                  props.handleVote(
+                    `upvote/?id=${props.post.pk}`,
+                    props.post.pk,
+                    1,
+                    e
+                  )
+                }}
+              >
                 +
               </button>
             </form>
           </div>
           <div class="input-group-prepend">
-            <form method="POST" action="downvote/?id={{ post.id }}">
-              <button type="submit" class="btn btn-danger">
+            <form method="GET">
+              <button
+                type="submit"
+                class="btn btn-danger"
+                onClick={e => {
+                  props.handleVote(
+                    `downvote/?id=${props.post.pk}`,
+                    props.post.pk,
+                    -1,
+                    e
+                  )
+                }}
+              >
                 -
               </button>
             </form>
@@ -89,6 +204,61 @@ function Main() {
   const [posts, setPosts] = useState([])
   const [displayedPosts, setDisplayedPosts] = useState([])
 
+  const cookies = new Cookies()
+
+  function handleVote(endpoint, id, value, event) {
+    event.preventDefault()
+    fetch(api + endpoint)
+      .then(res => {
+        if (res.status === 200) {
+          let newDisplay = Array.from(displayedPosts)
+          let singlePost = newDisplay.filter(post => post.pk === id)[0]
+          singlePost.fields.vote_count += value
+          setDisplayedPosts(newDisplay)
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  function handleFilterPosts(isBoast) {
+    const newDisplay = posts.filter(post => post.fields.is_boast === isBoast)
+    setDisplayedPosts(newDisplay)
+  }
+
+  function handleSort(ascending) {
+    const newDisplay = Array.from(displayedPosts)
+    if (ascending) {
+      newDisplay.sort(function(a, b) {
+        if (a.fields.vote_count < b.fields.vote_count) return 1
+        else if (a.fields.vote_count > b.fields.vote_count) return -1
+        else return 0
+      })
+    } else {
+      newDisplay.sort(function(a, b) {
+        if (a.fields.vote_count < b.fields.vote_count) return -1
+        else if (a.fields.vote_count > b.fields.vote_count) return 1
+        else return 0
+      })
+    }
+    setDisplayedPosts(newDisplay)
+  }
+
+  function handleNewPost(res) {
+    let newDisplay = Array.from(displayedPosts)
+    let newAllPosts = Array.from(posts)
+    newDisplay.unshift(res[0])
+    newAllPosts.unshift(res[0])
+    
+    setDisplayedPosts(newDisplay)
+    setPosts(newAllPosts)
+  }
+
+  function handleAllPosts() {
+    setDisplayedPosts(posts)
+  }
+
   useEffect(() => {
     fetch(api)
       .then(res => res.json())
@@ -96,6 +266,14 @@ function Main() {
         setPosts(data)
         setDisplayedPosts(data)
       })
+
+    if (cookies.get("csrf-token") === undefined) {
+      fetch(api + "token")
+        .then(res => res.json())
+        .then(data => {
+          cookies.set("csrf-token", data.csrfToken)
+        })
+    }
   }, [])
 
   return (
@@ -104,12 +282,17 @@ function Main() {
         <div class="col-10">
           <h1 class="mt-2">Ghost Post</h1>
           <hr class="mt-0 mb-4" />
-          <NavBar />
+          <NavBar
+            handleFilterPosts={handleFilterPosts}
+            handleSort={handleSort}
+            handleAllPosts={handleAllPosts}
+          />
+          <NewPostForm handleNewPost={handleNewPost} displayedPosts={displayedPosts}/>
           {displayedPosts.map(post => {
             return (
               <div class="card mb-3">
                 <PostHeader post={post} />
-                <PostBody post={post} />
+                <PostBody post={post} handleVote={handleVote} />
               </div>
             )
           })}
